16b6e27bf942cd49b6aaed370901f3b722b82221
Sarah Anduca <sarah.anduca@eurekalabs.com.br>
2022-10-04 23:43:39 -0300

    call api inside main component

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,6 +1,6 @@
-import Home from "../home/Home";
-import TopBall from "../../components/ball/Top";
 import BottomBall from "../../components/ball/Bottom";
+import TopBall from "../../components/ball/Top";
+import Home from "../home/Home";
 
 export default function Main() {
   return (
