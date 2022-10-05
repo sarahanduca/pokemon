4ea4332537ae055cc9ef93aabae482605aa8c3ad
Sarah Anduca <sarah.anduca@eurekalabs.com.br>
2022-10-04 23:45:00 -0300

    add object to pokemon

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import api from "../../api/api";
-export default function SearchBar() {
+
+export default function SearchBar(props) {
   const [search, setSearch] = useState("");
-  const [pokemon, setPokemon] = useState("");
+  const { setPokemon } = props;
 
   const handleSearch = async (e) => {
-    const pokemonData = await api(search);
-    console.log(pokemonData);
-    setPokemon(pokemonData.height);
+    const pokemonData = await api(search.toLowerCase());
+    setPokemon({
+      name: pokemonData.name,
+      img: pokemonData.sprites.front_default,
+      type: pokemonData.types.map((type) => type.type.name),
+      stats: {
+        hp: pokemonData.stats[0].base_stat,
+        attack: pokemonData.stats[1].base_stat,
+        defense: pokemonData.stats[2].base_stat,
+        speed: pokemonData.stats[5].base_stat,
+      },
+      abilities: pokemonData.abilities.map((ability) => ability.ability.name),
+      id: pokemonData.id,
+    });
   };
   return (
     <div className="searchBar justify-center flex">
@@ -20,7 +32,7 @@ export default function SearchBar() {
         onChange={(e) => setSearch(e.target.value)}
       />
       <button
-        type="button "
+        type="button"
         className="bg-neutral-200 hover:bg-yellow-400 border border-neutral-300 rounded-md focus:outline-none focus:border-neutral-500 focus:ring-neutral-900 focus:ring-1 py-1 px-3 h-1/2 mt-8"
         onClick={handleSearch}
       >
