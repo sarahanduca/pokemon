39c38fe44239f47f1d51619e54ce48ae6ccf81d8
Sarah Anduca <sarah.anduca@eurekalabs.com.br>
2022-10-08 18:25:26 -0300

    Fix pagination logic

--- src/components/showAllPokemons/ShowAllPokemons.jsx
+++ src/components/showAllPokemons/ShowAllPokemons.jsx
@@ -4,23 +4,22 @@ import api from "../../api/api";
 export default function ShowAllPokemons() {
   const [pokemonsData, setPokemonsData] = useState([]);
   const [paginationUrl, setPaginationUrl] = useState({
-    curr: 0,
-    next: 0,
-    prev: 0,
+    curr: "",
+    next: "?offset=20&limit=20",
+    prev: "",
   });
 
   const handleShowAllPokemons = async (currPage) => {
-    console.log(paginationUrl.next, paginationUrl.prev);
-    console.log(currPage);
     const allPokemonsData = await api(currPage);
     setPokemonsData(allPokemonsData.results);
     setPaginationUrl({
-      curr: (paginationUrl.curr += 20),
-      prev: paginationUrl.next > 0 ? (paginationUrl.curr -= 20) : 0,
-      next: (paginationUrl.curr += 20),
+      prev: allPokemonsData.previous
+        ? allPokemonsData.previous.split("/")[6]
+        : null,
+      curr: currPage,
+      next: allPokemonsData.next.split("/")[6],
     });
   };
-  console.log(paginationUrl.next, paginationUrl.prev);
   return (
     <div>
       <button
@@ -34,17 +33,13 @@ export default function ShowAllPokemons() {
       ))}
       <button
         className="bg-blue-400 border-1 p-1 mt-2 mr-6 text-white"
-        onClick={() =>
-          handleShowAllPokemons(`?offset=${paginationUrl.prev}&limit=20"`)
-        }
+        onClick={() => handleShowAllPokemons(paginationUrl.prev)}
       >
         prev
       </button>
       <button
         className="bg-blue-400 border-1 p-1 mt-2 text-white"
-        onClick={() =>
-          handleShowAllPokemons(`?offset=${paginationUrl.next}&limit=20"`)
-        }
+        onClick={() => handleShowAllPokemons(paginationUrl.next)}
       >
         next
       </button>
