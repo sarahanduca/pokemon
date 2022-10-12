8bd5291d33b4610d5bb310537c1762ccf3ae19dc
Sarah <anduca.sarah@gmail.com>
2022-10-12 13:37:03 -0300

    Call pokemons on first page render

--- src/components/showAllPokemons/ShowAllPokemons.jsx
+++ src/components/showAllPokemons/ShowAllPokemons.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { get } from "../../api/api";
 import Card from "../../components/card/Card";
 import { handleSearch } from "../../components/searchBar/SearchBar";
+import Button from "../simpleButton/SimpleButton";
 
 export default function ShowAllPokemons(props) {
-  const { pokemon, setPokemon } = props;
+  const { setPokemon } = props;
+  const [pokemonList, setPokemonList] = useState([]);
   const [paginationUrl, setPaginationUrl] = useState({
     curr: "",
     next: "?offset=20&limit=20",
     prev: "",
   });
-  const [pokemonList, setPokemonList] = useState([pokemon]);
+
+  useEffect(() => {
+    handleShowPokemons("");
+  }, []);
 
   const handlePagination = (prev, curr, next) => {
     setPaginationUrl({
-      prev: prev ? prev.split("/")[6] : null,
+      prev: prev ? prev.split("/")[6] : "",
       curr: curr,
       next: next.split("/")[6],
     });
@@ -26,41 +31,39 @@ export default function ShowAllPokemons(props) {
     const { results } = allPokemonsData;
     results.forEach(async (pokemon) => {
       const currPokemon = await handleSearch(pokemon.name, setPokemon);
-      setPokemonList((pokemonList)=>[...pokemonList, currPokemon]);
+      setPokemonList((pokemonList) => [...pokemonList, currPokemon]);
     });
     handlePagination(allPokemonsData.previous, currPage, allPokemonsData.next);
-    console.log(pokemonList)
   };
-  return (
-    <div>
-      <button
-        className="bg-green-400 border-1 p-1 mt-2 text-white"
-        onClick={() => handleShowPokemons("")}
-      >
-        pokemons
-      </button>
 
-      <div className="grid grid-cols-4 gap-8">
-        {pokemonList.length > 0 ? pokemonList.map((listPokemon) =>{
-          return (<div className="flex justify-center">
-            <Card pokemon={listPokemon}/>
-          </div>)
-        }
-          ) : null}
+  return (
+    <div className="displayPokemons">
+      <div className="pokemonCards grid grid-cols-4 gap-8">
+        {pokemonList.length > 0
+          ? pokemonList.map((listPokemon) => {
+              return (
+                <div className="flex justify-center">
+                  <Card pokemon={listPokemon} />
+                </div>
+              );
+            })
+          : null}
       </div>
 
-      <button
-        className="bg-blue-400 border-1 p-1 mt-2 mr-6 text-white"
-        onClick={() => handleShowPokemons(paginationUrl.prev)}
-      >
-        prev
-      </button>
-      <button
-        className="bg-blue-400 border-1 p-1 mt-2 text-white"
-        onClick={() => handleShowPokemons(paginationUrl.next)}
-      >
-        next
-      </button>
+      <div className="buttons flex justify-between mx-6 mt-4">
+        <Button
+          hidden={paginationUrl.prev == "" ? true : false}
+          text="previous"
+          colorBg="bg-yellow-400"
+          colorShadow="shadow-yellow-400/40"
+          onClick={() => handleShowPokemons(paginationUrl.prev)}
+        />
+
+        <Button
+          text="next"
+          onClick={() => handleShowPokemons(paginationUrl.next)}
+        />
+      </div>
     </div>
   );
 }
