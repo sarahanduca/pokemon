5df45398b419be86244b7cc522e67376b801a470
Sarah <anduca.sarah@gmail.com>
2022-10-13 17:37:44 -0300

    Centralize pokeball

--- src/components/ball/Bottom.jsx
+++ src/components/ball/Bottom.jsx
@@ -1,7 +1,7 @@
 export default function BottomBall() {
   return (
     <div className="relative bottomBall h-20 bg-neutral-100 border-t-4 border-neutral-900 ">
-      <div className="absolute bottom-2/3 left-1/2 h-12 w-12 ballCircle bg-neutral-200 bg-neutral-300 rounded-full border-2 drop-shadow-md border-neutral-900">
+      <div className="absolute bottom-2/3 left-[calc(50%-1.5rem)] h-12 w-12 ballCircle bg-neutral-200 bg-neutral-300 rounded-full border-2 drop-shadow-md border-neutral-900">
         {/* <div className="absolute top-5 right-0 h-6 w-12 ballCircleInsider rounded-bl-full rounded-br-full bg-white border-4 border-neutral-900"></div> */}
       </div>
     </div>
