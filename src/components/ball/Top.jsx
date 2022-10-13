2047576443a39f641141f5dac4519eb219e4b0b3
Sarah <anduca.sarah@gmail.com>
2022-10-13 17:37:57 -0300

    Centralize pokeball

--- src/components/ball/Top.jsx
+++ src/components/ball/Top.jsx
@@ -1,7 +1,7 @@
 export default function TopBall() {
   return (
-    <div className="relative topBall h-20 bg-red-600 border-b-4 border-neutral-900 ">
-      <div className="absolute top-2/3 left-1/2 h-12 w-12 ballCircle rounded-full bg-neutral-200 border-4 border-neutral-900">
+    <div className="relative topBall h-20 bg-red-600 border-b-4 border-neutral-900">
+      <div className="absolute top-2/3 left-[calc(50%-1.5rem)] h-12 w-12 ballCircle rounded-full bg-neutral-200 border-4 border-neutral-900">
         <div className="absolute top-2 left-2 h-6 w-6 ballCircleInsider rounded-full bg-white border-2 border-neutral-900"></div>
       </div>
     </div>
